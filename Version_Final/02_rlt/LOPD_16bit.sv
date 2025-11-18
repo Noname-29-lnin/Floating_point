36ba77ba3e6ba45e68236fa714a8e0c121256859
My name is Noname <dong.nguyendai29k22@hcmut.edu.vn>
2025-11-18 11:12:06 +0700

    update drawio

--- Version_Final/02_rlt/LOPD_16bit.sv
+++ Version_Final/02_rlt/LOPD_16bit.sv
@@ -34,6 +34,7 @@ assign w_o_pos_one[3] = w_zero_flag_1;
 assign w_o_pos_one[2] = w_zero_flag_1 ? w_pos_one_0[2] : w_pos_one_1[2];
 assign w_o_pos_one[1] = w_zero_flag_1 ? w_pos_one_0[1] : w_pos_one_1[1];
 assign w_o_pos_one[0] = w_zero_flag_1 ? w_pos_one_0[0] : w_pos_one_1[0];
-assign o_pos_one = o_zero_flag ? '0 : w_o_pos_one;
+// assign o_pos_one = o_zero_flag ? '0 : w_o_pos_one;
+assign o_pos_one = w_o_pos_one;
 
 endmodule
