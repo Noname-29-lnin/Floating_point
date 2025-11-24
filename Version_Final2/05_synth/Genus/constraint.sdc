668004253e9c1e3054f993d0e9c9a0954871ca31
Noname-29-lnin <128560496+Noname-29-lnin@users.noreply.github.com>
2025-11-24 09:27:01 +0700

    up

--- Version_Final2/05_synth/Genus/constraint.sdc
+++ Version_Final2/05_synth/Genus/constraint.sdc
@@ -1,4 +1,4 @@
-set_max_delay 2.0 -from [all_inputs] -to [all_outputs]
-set_input_delay 0.1 -clock [get_clocks *] [all_inputs]
-set_output_delay 0.1 -clock [get_clocks *] [all_outputs]
+set_max_delay 8.5 -from [all_inputs] -to [all_outputs]
+set_input_delay 0.0 -clock [get_clocks *] [all_inputs]
+set_output_delay 0.0 -clock [get_clocks *] [all_outputs]
 set timing_report_unconstrained true
--- Version_Final2/05_synth/Genus/genus_synth.tcl
+++ Version_Final2/05_synth/Genus/genus_synth.tcl
@@ -1,77 +1,74 @@
+##################################################
+# Khai Pham 
+# HCMUT LAB 203 
+##################################################
+set Top_module  FPU_unit
+set LIB_DIR     "./../PDK45nm/gpdk045_lib"
+set LIB_NAME    "slow_vdd1v0_basicCells_hvt"
+set LIB_FILE    "${LIB_DIR}/${LIB_NAME}.lib"
+set SDC_FILE    "./constraint.sdc"
+set SRC_FILE    "./../../02_rtl/"
+if {![file exists ${LIB_FILE}]} {
+    error "ERROR: Library file not found: ${LIB_FILE}"
+}
+put "TOP_MODULE = $Top_module"
+put "LIB_DIR = $LIB_DIR"
+put "LIB_NAME = $LIB_NAME"
+put "LIB_FILE = $LIB_FILE"
+put "SDC_FILE = $SDC_FILE"
+put "SRD_FILE = $SRC_FILE"
 
-##################################################
-#
-#Khai Pham 
-#HCMUT LAB 203 
-#
-##################################################
-#file setup variables#
-set top_module FPU_unit
-#set_db auto_ungroup none
-##################################################
-#Libraries setup#
-set_db library [list ./../slow_vdd1v2_basicCells_lvt.lib ]
-# set_db lef_library [ list /home/noname/Documents/project_tiny/PDKs/skywater130/tech/sky130_fd_sc_hd__nom.tlef ]
-##################################################
-#HDL files read#
-read_hdl -sv -f ./flist.f
-##################################################
+read_libs ${LIB_FILE}
+read_hdl -sv [glob ${SRC_FILE}*.sv]
+# read_hdl -sv -f ./flist.f
 elaborate
-set_top_module ${top_module}
-write_hdl > ./Genus/${top_module}_elab.v
+set_top_module ${Top_module}
+
+set elab_v "./${LIB_NAME}/${Top_module}_elab.v"
+write_hdl > ${elab_v}
 check_design -all
-##################################################
-# #Timing constraint variables#
-# set FREQ_GHz 0.05
-# set FREQ [ expr ${FREQ_GHz} * 1000000000.0 ]
-# set PERIOD_tmp [ expr (1.0/${FREQ}) ]
-# set PERIOD [ expr ${PERIOD_tmp} * 1000000000000.0 ]
-# puts "Clock Period = ${PERIOD} ps"
-# set IN_DLY [ expr $PERIOD/2.0 ]
-# set OUT_DLY [ expr $PERIOD/2.0 ]
-# set UNCER [expr ${PERIOD}/100000.0]
-# puts "Clock Uncertainty = ${UNCER} ns"
-# set TRANS 1.5
-# puts "max transition = ${TRANS} ns"
-# ##################################################
-# #set constraint for clock and input with output#
-# set clock [define_clock -period $PERIOD -name clk [clock_ports] ]
-# set_dont_touch_network [get_clocks clk]
-# set_clock_uncertainty ${UNCER} -setup -hold clk
-# set_max_transition ${TRANS} [get_design ${top_module}] 
-# external_delay -clock clk -input $IN_DLY  -name delay_in [all_inputs]
-# external_delay -clock clk -output $OUT_DLY -name delay_out [all_outputs]
-read_sdc ./constraint.sdc
-##################################################
-#Synthesize RTL code to generic#
+
+read_sdc ${SDC_FILE}
+
+set_db syn_generic_effort medium
+set_db syn_map_effort medium 
+set_db syn_opt_effort medium
+
 syn_generic
-write_hdl > ./Genus/${top_module}_generic.v
-#Mapping technology to the generic#
+set generic_v "./${LIB_NAME}/${Top_module}_generic.v"
+write_hdl > ${generic_v}
+
 syn_map
-write_hdl > ./Genus/${top_module}_tech_map.v
-#remove assignment statement 
+set techmap_v "./${LIB_NAME}/${Top_module}_tech_map.v"
+write_hdl > $techmap_v
+
 remove_assigns_without_optimization -verbose
-#Optimizing the mapped technology netlist#
-syn_opt  
+syn_opt
 
 report timing -lint
-##################################################
-#design post-synthesis export#
-#Export the netlist 
-write_hdl > ./Genus/${top_module}_netlist.v
-#Export the standard delay format of the synthesized design #
-write_sdf -edges check_edge -setuphold "split" -recrem split > ./Genus/${top_module}.sdf
+set netlist_v "./${LIB_NAME}/${Top_module}_netlist.v"
+write_hdl > ${netlist_v}
+
+set sdf_out "./${LIB_NAME}/${Top_module}.sdf"
+write_sdf -timescale ns -nonegchecks -recrem split -edges check_edge -setuphold split  > $sdf_out
+# write_sdf -edges check_edge -setuphold "split" -recrem split > $sdf_out
+
+report_timing -max_paths 1 > ./${LIB_NAME}/report/${Top_module}_timing.rpt
+report hierarchy > ./${LIB_NAME}/report/${Top_module}_hier.rpt
+report gates > ./${LIB_NAME}/report/${Top_module}_gates.rpt
+report datapath > ./${LIB_NAME}/report/${Top_module}_datapath.rpt
+report_qor > ./${LIB_NAME}/report/${Top_module}_qor.rpt
+report_area > ./${LIB_NAME}/report/${Top_module}_area.rpt
+report_power > ./${LIB_NAME}/report/${Top_module}_power.rpt
+
+puts "Done. Using library: $LIB_FILE"
+puts "Elaborated    -> $elab_v"
+puts "Generic       -> $generic_v"
+puts "Tech-mapped   -> $techmap_v"
+puts "Netlist       -> $netlist_v"
+puts "SDF           -> $sdf_out"
+puts "Reports       -> ./${LIB_NAME}/report/"
 
 ##################################################
-#Reports export#
-report timing -max_paths 10 > ./Genus/report/${top_module}.timing.rpt
-report hierarchy > ./Genus/report/${top_module}.hier.rpt
-report gates > ./Genus/report/${top_module}.gates.rpt
-report datapath > ./Genus/report/${top_module}.datapath.rpt
-report qor > ./Genus/report/${top_module}.qor.rpt
-report area > ./Genus/report/${top_module}.area.rpt
-report power > ./Genus/report/${top_module}.power.rpt
-##################################################
-#Open genus GUI 
+# Open genus GUI (tuỳ chọn)
 gui_show
-
